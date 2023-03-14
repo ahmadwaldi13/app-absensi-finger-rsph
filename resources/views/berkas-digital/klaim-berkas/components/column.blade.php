482a47c66d3b1f34b89d75dfdc6a57f984fa9e7f
jiwana <jiwana.programmer@gmail.com>
2023-03-14 18:45:31 +0700

    perapian

--- resources/views/berkas-digital/klaim-berkas/components/column.blade.php
+++
@@ -1,7 +0,0 @@
-<table cellspacing="0" cellpadding="0">
-    <tr>
-        <td width="35%">{{$title}}</td>
-        <td width="2%" align="center">:</td>
-        <td width="64%">{{$data}}</td>
-    </tr>
-</table>
